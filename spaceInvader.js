c80befd3c20e1f646179e7913ca8d8e795dea040
Thomas Heusdens <thomas.heusdens@student.ehb.be>
2022-10-07 19:15:03 +0200

    assignment 1 spaceinvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,76 @@
 "use strict";
 
-alert("Draw your space invader here");
+DrawSpaceInvader();
+
+function DrawSpaceInvader() {
+
+    let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+    context.fillStyle = 'black'
+    context.beginPath();
+    context.rect(50,50,300,300);
+    context.fill();
+
+    //left side
+    context.fillStyle = '#1CFF00'
+    context.beginPath();
+    context.rect(75,75,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(125,75,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(75,125,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(75,175,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(125,225,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(125,275,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(75,275,50,50);
+    context.fill();
+
+    //right side
+    context.beginPath();
+    context.rect(225,75,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(275,75,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(275,125,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(275,175,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(225,225,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(225,275,50,50);
+    context.fill();
+
+    context.beginPath();
+    context.rect(275,275,50,50);
+    context.fill();
+}
+
